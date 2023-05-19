5b3478330587a491986c141e99665a8ffdab2f6c
Renato Cordova <ren99cordova@gmail.com>
2023-05-19 13:46:12 -0400

    finished stories component

---
+++ data/posts.js
@@ -0,0 +1,103 @@
+import { USERS } from "./users";
+
+export const POSTS = [
+  {
+    imageurl:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*",
+    user: USERS[0].user,
+    likes: 1120,
+    caption: "Idk Help Me :)",
+    profile_pic: USERS[0].image,
+    date: 5,
+    comments: [
+      {
+        user: "Dumb",
+        comment: "Shit Pic",
+      },
+      {
+        user: "Hathi",
+        comment: "You Are Dumb",
+      },
+    ],
+  },
+  {
+    imageurl:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*",
+    user: USERS[4].user,
+    likes: 4324,
+    caption:
+      "i am dumb you are also so hello bye haha btw you are also dumb hahah",
+    profile_pic: USERS[4].image,
+    date: 69,
+    comments: [
+      {
+        user: "Nerd",
+        comment: "DumbNerd",
+      },
+    ],
+  },
+  {
+    imageurl:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*",
+    user: USERS[4].user,
+    likes: 4324,
+    caption:
+      "i am dumb you are also so hello bye haha btw you are also dumb hahah",
+    profile_pic: USERS[4].image,
+    date: 69,
+    comments: [
+      {
+        user: "Nerd",
+        comment: "DumbNerd",
+      },
+    ],
+  },
+  {
+    imageurl:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*",
+    user: USERS[4].user,
+    likes: 4324,
+    caption:
+      "i am dumb you are also so hello bye haha btw you are also dumb hahah",
+    profile_pic: USERS[4].image,
+    date: 69,
+    comments: [
+      {
+        user: "Nerd",
+        comment: "DumbNerd",
+      },
+    ],
+  },
+  {
+    imageurl:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*",
+    user: USERS[4].user,
+    likes: 4324,
+    caption:
+      "i am dumb you are also so hello bye haha btw you are also dumb hahah",
+    profile_pic: USERS[4].image,
+    date: 69,
+    comments: [
+      {
+        user: "Nerd",
+        comment: "DumbNerd",
+      },
+    ],
+  },
+  {
+    imageurl:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*",
+    user: USERS[4].user,
+    likes: 4324,
+    caption:
+      "i am dumb you are also so hello bye haha btw you are also dumb hahah",
+    profile_pic: USERS[4].image,
+    date: 69,
+    comments: [
+      {
+        user: "Nerd",
+        comment: "DumbNerd",
+      },
+    ],
+  },
+];
---
+++ data/users.js
@@ -0,0 +1,27 @@
+export const USERS = [
+  {
+    user: "Varun",
+    image:
+      "https://preview.redd.it/v8z0crk5gho71.png?width=640&crop=smart&auto=webp&s=8afbdf5f25c33718c810de00f40cb267ef9ae469",
+  },
+  {
+    user: "VarunNerdd",
+    image:
+      "https://preview.redd.it/v8z0crk5gho71.png?width=640&crop=smart&auto=webp&s=8afbdf5f25c33718c810de00f40cb267ef9ae469",
+  },
+  {
+    user: "Varun",
+    image:
+      "https://preview.redd.it/v8z0crk5gho71.png?width=640&crop=smart&auto=webp&s=8afbdf5f25c33718c810de00f40cb267ef9ae469",
+  },
+  {
+    user: "Elephant",
+    image:
+      "https://cdn2.vectorstock.com/i/1000x1000/28/71/cartoon-funny-elephant-isolated-vector-7402871.jpg",
+  },
+  {
+    user: "Sur",
+    image:
+      "https://st3.depositphotos.com/1036149/13004/i/950/depositphotos_130046402-stock-photo-funny-pig-doing-yoga.jpg",
+  },
+];
